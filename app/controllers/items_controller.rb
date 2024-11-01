e27226c4ff4c8591677d1c0d222fdd23b01089b3
ktung4 <ktung@chicagobooth.edu>
2024-11-01 13:22:57 +0000

    Link_in_bio_db

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,8 @@
+# app/controllers/items_controller.rb
+
+class ItemsController < ApplicationController
+  def index
+    @list_of_items = Item.all
+    render({ :template => "item_templates/list" })
+  end
+end
